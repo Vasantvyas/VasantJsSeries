75df32692b13ee15c757c03c5259fa91635fd943
Vasantvyas <144471587+Vasantvyas@users.noreply.github.com>
2023-09-08 22:52:29 +0000

    Basic Understanding of Variables in Js

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountstate = "maharashtra"
+var accountCity = "mumbai"
+city = "mumbai"
+
+
+ // js doesn't alllow to reasign constant, will through Type Error.
+ //example.....  accountId = 123454
+/* 
+in javascript, if you declare the variable but did not assigned value...
+ then js assign  undefined value tp that variable. Example: District
+note: we can change the value of let and var. using assignment.
+ */
+let District;
+
+ accountCity = "Bengluru"
+
+console.table([accountId,accountCity,accountstate,city,District])
