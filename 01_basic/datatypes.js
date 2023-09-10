2c39670966a55641889e5900fc549f51bb1d03e9
Vasantvyas <144471587+Vasantvyas@users.noreply.github.com>
2023-09-10 15:22:40 +0000

    added by vasant vyas 09102023

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,10 @@
+console.log(typeof null);
+let name = "vasant"
+let age = 25
+let location = 4.45
+let count = 1.22223333333
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof location);
+console.log(typeof count);
