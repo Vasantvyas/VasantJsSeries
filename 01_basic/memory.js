ba07ec6f5364522b7f167afccb8cd68f2f4bf68e
Vasantvyas <144471587+Vasantvyas@users.noreply.github.com>
2023-09-19 15:19:05 +0000

    changes

--- 01_basic/memory.js
+++ 01_basic/memory.js
@@ -1,10 +1,31 @@
+// Exp:   primitive datatype
+
 let names = "vasant vyas"
 // console.log(names);
 let abc = names
 abc = "namste"
-console.log(names);
-console.log(abc);
+// console.log(names);
+// console.log(abc);
 
 
 
-names = "paliwak"
+// let xyz = Symbol(abc)
+// let abb = Symbol(abc)
+// console.log(xyz);
+// console.log(abb);
+
+
+// Exp:  non primitive datatype
+
+
+let userOne = {
+    id : "1",
+    name : "vasant vyas"
+}
+
+let userTwo = userOne
+userTwo.name="vasant paliwal"
+
+console.log(userOne);
+console.log(userTwo);
+
