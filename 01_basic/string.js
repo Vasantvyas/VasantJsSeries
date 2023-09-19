29bca8c967bc00d660f9d7834255ad9ea4e5d51a
Vasantvyas <144471587+Vasantvyas@users.noreply.github.com>
2023-09-19 18:04:19 +0000

    string

---
+++ 01_basic/string.js
@@ -0,0 +1,18 @@
+let a = "vasant"
+let b = "vyas"
+
+// console.log(`hy my name is ${a} and my sirnamr is ${b}`);
+
+console.log(a)
+
+// console.log(a.toUpperCase());
+// console.log(a.charAt('l'));
+
+const newstring = a.substring(0,4)
+console.log(newstring);
+console.log(a.slice(-2,0));
+
+let bb = "    vasant g v      "
+console.log(bb.trim());
+
+console.log(bb.replace('g','Ganeshlal'));
