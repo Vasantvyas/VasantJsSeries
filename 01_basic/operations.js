3159be861bea98e6f786adda9f7b62456dd2c46b
Vasantvyas <144471587+Vasantvyas@users.noreply.github.com>
2023-09-19 06:08:50 +0000

    operations19092023

---
+++ 01_basic/operations.js
@@ -0,0 +1,20 @@
+let value = 3
+let negvalue = -value
+
+//console.log(negvalue);
+
+// 
+
+let str1 = "hello "
+let str2 = "vasant"  
+let str3 = str1 - str2
+console.log(str3);
+////output:  NaN 
+
+
+let str4 = "hello "
+let str5 = "vasant"  
+let str6 = str4 + str5
+console.log(str6);
+////output:  hello vasant 
+
