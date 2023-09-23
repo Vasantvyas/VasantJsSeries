57126d64260c755dede9af621e91f602ace85089
Vasantvyas <144471587+Vasantvyas@users.noreply.github.com>
2023-09-23 14:46:12 +0000

    functions

---
+++ 01_basic/02_basics/functionspread.js
@@ -0,0 +1,13 @@
+function createcared(val1,val2,...num){
+    return num
+}
+
+// console.log(createcared(1,2,3,4,5));
+
+const myarray = [1,2,3,4,5]
+
+function returnsecondvaluefromarray(aaa){
+    return aaa[1]
+}
+
+console.log(returnsecondvaluefromarray(myarray));
