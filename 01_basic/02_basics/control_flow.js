f43081d2f82889c89290931b87f6487613a5cc0d
Vasantvyas <144471587+Vasantvyas@users.noreply.github.com>
2023-12-14 15:42:47 +0000

    fffff

---
+++ 01_basic/02_basics/control_flow.js
@@ -0,0 +1,52 @@
+// if (true){
+//     console.log("happy coding");
+// }
+
+// if (2==="2"){
+//     console.log('equal to 2 __1');
+// }
+
+// if (2 =="1"){
+//     console.log('equal to 2 ___2');
+// } else {
+//     console.log('not equal to 2 ___2');
+// }
+
+// const fly = "200";
+
+// if (fly>100){
+//     console.log(`this ${fly} is greater than 100`);
+// }
+
+// syntax for swich case:
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 1
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("may");
+        break;
+
+    default:
+        console.log("default case");
+        break;
+}
+
+// note: if hum case ke baad break nhi likhte 
+// to true cases ke baadke sare cases bina kisi check ke true ho jayege, exceptdefault case.
